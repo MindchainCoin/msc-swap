da1a9e83bbf542f0c1fc6fb3e963154bb4dff25c
nnlgsakib <nlgarts@outlook.com>
2024-06-23 11:17:45 +0600

    removed ens support

--- src/utils/getTokenList.ts
+++ src/utils/getTokenList.ts
@@ -1,8 +1,6 @@
 import { TokenList } from '@uniswap/token-lists'
 import schema from '@uniswap/token-lists/src/tokenlist.schema.json'
 import Ajv from 'ajv'
-import contenthashToUri from './contenthashToUri'
-import { parseENSAddress } from './parseENSAddress'
 import uriToHttp from './uriToHttp'
 
 // bakeryswap defaultTokenJson
@@ -14,37 +12,16 @@ const tokenListValidator = new Ajv({ allErrors: true }).compile(schema)
 /**
  * Contains the logic for resolving a list URL to a validated token list
  * @param listUrl list url
- * @param resolveENSContentHash resolves an ens name to a contenthash
  */
 export default async function getTokenList(
-  listUrl: string,
-  resolveENSContentHash: (ensName: string) => Promise<string>
+  listUrl: string
 ): Promise<TokenList> {
   if (listUrl === DEFAULT_TOKEN_LIST_URL) {
     return defaultTokenJson
   }
-  const parsedENS = parseENSAddress(listUrl)
 
-  let urls: string[]
-  if (parsedENS) {
-    let contentHashUri
-    try {
-      contentHashUri = await resolveENSContentHash(parsedENS.ensName)
-    } catch (error) {
-      console.error(`Failed to resolve ENS name: ${parsedENS.ensName}`, error)
-      throw new Error(`Failed to resolve ENS name: ${parsedENS.ensName}`)
-    }
-    let translatedUri
-    try {
-      translatedUri = contenthashToUri(contentHashUri)
-    } catch (error) {
-      console.error('Failed to translate contenthash to URI', contentHashUri)
-      throw new Error(`Failed to translate contenthash to URI: ${contentHashUri}`)
-    }
-    urls = uriToHttp(`${translatedUri}${parsedENS.ensPath ?? ''}`)
-  } else {
-    urls = uriToHttp(listUrl)
-  }
+  const urls = uriToHttp(listUrl)
+
   for (let i = 0; i < urls.length; i++) {
     const url = urls[i]
     const isLast = i === urls.length - 1
